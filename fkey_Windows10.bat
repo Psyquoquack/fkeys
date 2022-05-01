f2efabeade1e30b3b0364ef3a038004d5f42417e
Jacquouille la Fripouille <90555014+Psyquoquack@users.noreply.github.com>
2022-05-01 03:11:20 +0200

    py to bat

---
+++ fkey_Windows10.bat
@@ -0,0 +1,64 @@
+@echo off
+
+cls
+Echo FKEYS make for the good of the humanity ;)
+Echo Made By Jacquouille la Fripouille#4530
+Echo 1- Home
+Echo 2- Home N
+Echo 3- Home Single Language
+Echo 4- Home Country Specific
+Echo 5- Professional
+Echo 6- Professional N
+Echo 7- Education
+Echo 8- Education N
+Echo 9- Entreprise
+Echo 10- Entreprise N
+set /p ans="[1-10]:"
+
+if %ans%==1 (
+slmgr /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99
+goto z
+)
+if %ans%==2 (
+slmgr /ipk 3KHY7-WNT83-DGQKR-F7HPR-844BM
+goto z
+)
+if %ans%==3 (
+slmgr /ipk 7HNRX-D7KGG-3K4RQ-4WPJ4-YTDFH
+goto z
+)
+if %ans%==4 (
+slmgr /ipk PVMJN-6DFY6–9CCP6–7BKTT-D3WVR
+goto z
+)
+if %ans%==5 (
+slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+goto z
+)
+if %ans%==6 (
+slmgr /ipk MH37W-N47XK-V7XM9-C7227-GCQG9
+goto z
+)
+if %ans%==7 (
+slmgr /ipk NW6C2-QMPVW-D7KKK-3GKT6-VCFB2
+goto z
+)
+if %ans%==8 (
+slmgr /ipk 2WH4N-8QGBV-H22JP-CT43Q-MDWWJ
+goto z
+)
+if %ans%==9 (
+slmgr /ipk NPPR9-FWDCX-D2C8J-H872K-2YT43
+goto z
+)
+if %ans%==10 (
+slmgr /ipk DPH2V-TTNVB-4X9Q3-TJR4H-KHJW4
+goto z
+)
+
+:z
+slmgr /skms jacquouille.zapto.org
+slmgr /ato
+Echo Made By Jacquouille la Fripouille#4530
+start https://github.com/Psyquoquack
+cmd /k
